ba1a677ff3fb6adf65a39be7eac6c04cc0b1b886
CaioGiacomelli <caio.giacomelli@hotmail.com>
2017-09-28 17:36:11 -0300

    Troca de líder infinita e problema com set_timeout

---
+++ main.py
@@ -0,0 +1,207 @@
+import message
+from threading import Thread
+import threading
+import socket
+import pickle
+import copy
+import time
+import os
+from waiting import wait
+
+class MyThread (threading.Thread):
+
+    def __init__(self, pro, type):
+
+        threading.Thread.__init__(self)
+        self.pro = pro
+        self.type = type
+        # self.name = name
+        # self.counter = counter
+
+    def run(self):
+        send_request(self.pro, self.type)
+
+
+def send_request(pro, type):
+
+    if type == 2 or type == 3 or type == 4:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if type == 4:
+            tcp.connect((host, p[pro - 1].port))
+            m1 = message.Message(pro, type)
+        else:
+            tcp.connect((host, p[pro.lider - 1].port))
+            m1 = message.Message(pro.pid, type)
+        m_dumped = pickle.dumps(m1)
+        x = tcp.send(m_dumped)
+        print(x)
+        tcp.close()
+
+
+
+    elif type == 1:
+        for process in p:
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tcp.connect((host, process.port))
+            m1 = message.Message(pro.pid, type)
+            m_dumped = pickle.dumps(m1)
+            x = tcp.send(m_dumped)
+            tcp.close()
+    elif type == 0:
+        for process in p:
+            if pro.pid < process.pid:
+                tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                tcp.connect((host, process.port))
+                m1 = message.Message(pro.pid, type)
+                m_dumped = pickle.dumps(m1)
+                x = tcp.send(m_dumped)
+                tcp.close()
+
+
+
+class MyThread2 (threading.Thread):
+
+    def __init__(self, pr):
+
+        threading.Thread.__init__(self)
+        self.process = pr
+        # self.name = name
+        # self.counter = counter
+
+    def run(self):
+        Process.receive(self.process)
+
+class MyThread3 (threading.Thread):
+
+    def __init__(self, var):
+
+        threading.Thread.__init__(self)
+        self.var = var
+        # self.name = name
+        # self.counter = counter
+
+    def run(self):
+        Process.mtime(self.var)
+
+
+class Process:
+
+    def __init__(self, pid, host, port):
+        self.pid = pid
+        self.lider = 5
+        self.failed = 0
+        self.candidato = 0
+        self.sender = 0
+        self.process_list = []
+        self.host = host
+        self.port = port
+        thread2 = MyThread2(self)
+        thread2.start()
+
+    def set_process_list(self, process_list):
+        self.process_list = process_list
+
+    def set_failed(self, failed):
+        self.failed = failed
+
+
+    def tcp_accept(self, tcp):
+        return tcp.accept()
+
+    def receive(self):
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        orig = (self.host, self.port)
+        tcp.bind(orig)
+        tcp.listen(1)
+
+
+        while True:
+
+
+                try:
+                    tcp.settimeout(3)
+                    con, cliente = self.tcp_accept(tcp)
+                except socket.timeout:
+                    if self.sender:
+                        thread4 = MyThread(self, 0)
+                        thread4.start()
+                    if self.candidato:
+                        thread5 = MyThread(self, 1)
+                        thread5.start()
+                except:
+                    raise
+                else:
+
+                    if not self.failed:
+
+                        while True:
+                            print(self.failed, self.pid)
+                            msg = con.recv(1024)
+                            if msg:
+                                msg_loaded = pickle.loads(msg)
+                                new_m = copy.copy(msg_loaded)
+
+                            if not msg: break
+
+                        con.close()
+                        t = time.time()
+
+                        if new_m.type == 0:
+                            if new_m.pid < self.pid:
+                                thread3 = MyThread(self, 0)
+                                thread3.start()
+                                self.candidato = 1
+
+                        elif new_m.type == 1:
+                                self.lider = new_m.pid
+                                print("Eu processo ", self.pid, " concordo em ser liderado pelo processo ", self.lider)
+                        elif new_m.type == 2:
+                                print("Lider recebeu a mensagem do processo ", new_m.pid)
+
+                        elif new_m.type == 3:
+                                print("Recebeu a mensagem OK do processo ", new_m.pid)
+
+                        elif new_m.type == 4:
+                                self.sender = 0
+
+
+
+
+host = '192.168.0.105'
+process_number = 1
+p = [Process(process_number, host, 5000),
+     Process(process_number + 1, host, 5001),
+     Process(process_number + 2, host, 5002),
+     Process(process_number + 3, host, 5004),
+     Process(process_number + 4, host, 5005)]
+
+for proc in p:
+    proc.set_process_list(p)
+
+
+
+
+print("1 - Enviar Mensagem")
+print("2 - Sair")
+option = input()
+
+while True:
+
+
+    if option == '1':
+        print("Qual processo enviará a mensagem?")
+        sender = int(input())
+        p[sender-1].sender = 1
+        thread1 = MyThread(p[sender-1], 2)
+        thread1.start()
+    if option == '3':
+        p[4].set_failed(1)
+
+    elif option == '2':
+        os._exit(0)
+
+    print("1 - Enviar Mensagem")
+    print("2 - Sair")
+    option = input()
+
+
---
+++ message.py
@@ -0,0 +1,7 @@
+
+
+class Message:
+
+    def __init__(self, pid, type):
+        self.pid = pid
+        self.type = type
